e1c6c75650e295ce2a40992b83f39c569feeac76
Lucas Ferreira <lucasferreira9569@gmail.com>
2025-07-15 20:10:51 -0300

    Update LinkedList.h

--- Projetos em C/Lista Simplesmente Encadeada/LinkedList.h
+++ Projetos em C/Lista Simplesmente Encadeada/LinkedList.h
@@ -1,1 +1,23 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
 
+#include "Node.h"
+
+// Declaração da classe LinkedList, que gerencia as operações da lista.
+class LinkedList {
+private:
+    Node* topo;
+
+public:
+    // Construtor: inicializa uma lista vazia.
+    LinkedList();
+    // Destrutor: libera toda a memória alocada pela lista.
+    ~LinkedList();
+    void inserir(int data);
+    bool remover(int data);
+    bool buscar(int data) const;
+    void exibir() const;
+    bool estaVazia() const;
+};
+
+#endif //LINKEDLIST_H
