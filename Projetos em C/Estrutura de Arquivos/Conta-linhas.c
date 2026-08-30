b941c9893475e8e5dba5ae1b8e076886da33763d
Lucas Ferreira <lucasferreira9569@gmail.com>
2026-08-29 21:14:15 -0300

    Add line counting program in Conta-linhas.c

---
+++ Projetos em C/Estrutura de Arquivos/Conta-linhas.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    FILE *entrada;
+    int c;
+    int linhas = 0;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Erro na chamada do comando.\n");
+        fprintf(stderr, "Uso: %s [ARQUIVO]\n", argv[0]);
+        return 1;
+    }
+
+    entrada = fopen(argv[1], "rb");
+    if (!entrada)
+    {
+        fprintf(stderr, "Arquivo %s não pode ser aberto para leitura\n", argv[1]);
+        return 1;
+    }
+
+    c = fgetc(entrada);
+
+    while (c != EOF)
+    {
+        if (c == '\n')
+            linhas++;
+
+        c = fgetc(entrada);
+    }
+
+    fclose(entrada);
+
+    printf("Quantidade de linhas: %d\n", linhas);
+
+    return 0;
+}
